6d213db5b9b38658b1dff7eaa3a2796c535f48ec
isiji <blairisiji@gmail.com>
2024-05-02 14:53:40 +0300

    login page

--- web_flask/app.py
+++ web_flask/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """home route"""
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
