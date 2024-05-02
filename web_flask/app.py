c8f5294e1379ac3bde242054b6a49d221c176d79
isiji <blairisiji@gmail.com>
2024-05-02 15:13:49 +0300

    login page

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """flask application"""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ app = Flask(__name__)
 def home():
     """home route"""
     return render_template('home.html')
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    """login route"""
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
