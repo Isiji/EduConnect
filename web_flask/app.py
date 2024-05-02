0166f02a486ba2f1b147bd40d9af13486361ea11
isiji <blairisiji@gmail.com>
2024-05-02 19:16:17 +0300

    app.py

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """flask application"""
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, session, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-
+app.secret_key = 'secret_key'
 @app.route('/')
 def home():
     """home route"""
@@ -13,8 +13,25 @@ def home():
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
     """login route"""
-    return render_template('login.html')
-
-
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('home'))
+    else:
+        if user in session:
+            return redirect(url_for('home'))
+        
+        return render_template('login.html')
+@app.route('/logout/')
+def logout():
+    """logout route"""
+    session.pop('user', None)
+    return redirect(url_for('home'))
+@app.route('user')
+def user():
+    """user route"""
+    if "user" in session:
+        user = session['user']
+        return render_template('user.html')
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
