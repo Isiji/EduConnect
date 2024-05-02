5a057093ddb8f837bb75ebeb531f3b114cd3cb68
isiji <blairisiji@gmail.com>
2024-05-02 14:34:21 +0300

    creating html templates

---
+++ web_flask/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""flask application"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """home route"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
